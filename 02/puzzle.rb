aa382d20081975da3823e47fad5d941b7d5e0926
Brian <tachyons>
2022-12-02 19:28:47 -0800

    day 2 part 2

--- 02/puzzle.rb
+++ 02/puzzle.rb
@@ -1,18 +1,18 @@
-ROCK = 'X'
-PAPER = 'Y'
-SCISSORS = 'Z'
+ROCK = 'A'
+PAPER = 'B'
+SCISSORS = 'C'
 
-RPS = {
-  'A' => ROCK,
-  'B' => PAPER,
-  'C' => SCISSORS
-}
+LOSE = 'X'
+DRAW = 'Y'
+WIN = 'Z'
 
 WINNING_MOVES = {
   ROCK => SCISSORS,
   PAPER => ROCK,
   SCISSORS => PAPER
 }
+
+MOVE_TO_WIN = WINNING_MOVES.invert
 
 SCORES = {
   ROCK => 1,
@@ -22,27 +22,16 @@ SCORES = {
 
 input = File.readlines('input').map(&:strip).reject(&:empty?)
 
-def win?(move1, move2)
-  WINNING_MOVES[move2] == RPS[move1]
-end
-
-def draw?(move1, move2)
-  move2 == RPS[move1]
-end
-
-def lose?(move1, move2)
-  WINNING_MOVES[RPS[move1]] == move2
-end
-
 score = 0
 input.each do |line|
-  move1, move2 = line.split(' ')
-  if win?(move1, move2)
-    score = score + 6 + SCORES[move2]
-  elsif draw?(move1, move2)
-    score = score + 3 + SCORES[move2]
-  elsif lose?(move1, move2)
-    score = score + 0 + SCORES[move2]
+  move, result = line.split(' ')
+
+  if result == WIN
+    score = score + 6 + SCORES[MOVE_TO_WIN[move]]
+  elsif result == DRAW
+    score = score + 3 + SCORES[move]
+  elsif result == LOSE
+    score += SCORES[WINNING_MOVES[move]]
   end
 end
 

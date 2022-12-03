fb278a6d2274513ce2fdf71ba6e2a3878f7320b6
Brian <tachyons>
2022-12-02 21:41:34 -0800

    day 3 p2

--- 03/puzzle.rb
+++ 03/puzzle.rb
@@ -3,13 +3,10 @@ input = File.readlines('input').map(&:strip).reject(&:empty?)
 priorities = (('a'..'z').zip(1..26) + ('A'..'Z').zip(27..52)).to_h
 
 sum = 0
-input.each do |line|
-  rucksack = line.split('')
-  length = rucksack.length
-  one = rucksack.take(length/2)
-  two = rucksack.drop(length/2)
+input.each_slice(3) do |lines|
+  one, two, three = lines.map { |line| line.split('') }
+  overlap = (one & two) & three
 
-  overlap = one & two
   sum += priorities[overlap.first]
 end
 p sum

a3f885ab4920570907695517d771a18efd0994d7
Brian <tachyons>
2022-12-03 21:24:58 -0800

    day4 p2

--- 04/puzzle.rb
+++ 04/puzzle.rb
@@ -6,7 +6,7 @@ input.each do |line|
   a,b = pair1.split('-').map(&:to_i)
   x,y = pair2.split('-').map(&:to_i)
 
-  count += 1 if (a.between?(x,y) && b.between?(x,y)) || (x.between?(a,b) && y.between?(a,b))
+  count += 1 if a.between?(x,y) || b.between?(x,y) || x.between?(a,b) || y.between?(a,b)
 end
 
 pp count

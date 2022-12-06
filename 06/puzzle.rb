4588df6ef5b79d08d2316d972142f22b5b62fa96
Brian <tachyons>
2022-12-06 00:48:53 -0800

    day 6 p1

---
+++ 06/puzzle.rb
@@ -0,0 +1,22 @@
+input = File.readlines('input').map &:strip
+
+uniqs = []
+index = nil
+
+input[0].chars.each_with_index do |char, i|
+  if uniqs.include? char
+    # drop everything up to and including the first dup from uniqs list
+    dup_index = uniqs.find_index(char)
+    uniqs = uniqs.drop(dup_index+1)
+    index = nil
+  end
+
+  uniqs << char
+
+  if uniqs.length == 4
+    index = i
+    break
+  end
+end
+
+p index + 1

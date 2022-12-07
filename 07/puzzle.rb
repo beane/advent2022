ea0b19966e43fce436b3057e496be79ddd107748
Brian <tachyons>
2022-12-07 09:52:14 -0800

    day 7 p1

---
+++ 07/puzzle.rb
@@ -0,0 +1,95 @@
+class Node
+  attr_accessor :name, :parent, :children
+
+  def initialize(name, parent=nil)
+    @name = name
+    @parent = parent
+    @children = []
+  end
+
+  def size
+    raise NotImplementedError.new 'call size on DirNode or FileNode instance instead'
+  end
+
+  def directory?; false; end
+  def file?; false; end
+end
+
+class DirNode < Node
+  def size
+    children.map { |node| node.size }.sum # might not work :)
+  end
+
+  def to_s
+    "DirNode (name: #{name} | parent: #{parent.name rescue 'parentless'})"
+  end
+
+  def directory?; true; end
+end
+
+class FileNode < Node
+  attr_accessor :size
+
+  def initialize(name, parent, size)
+    @size = size
+    super(name, parent)
+  end
+
+  def to_s
+    "FileNode (name: #{name} | parent: #{parent.name rescue 'parentless'})"
+  end
+
+  def file?; true; end
+end
+
+input = File.readlines('input').map &:strip
+root = DirNode.new('/')
+current = root
+
+input.each do |line|
+  # parse command
+  if line[0] == '$'
+    # if cd
+    #   change the node we're looking at
+    # if ls
+    #   nothing :)
+    #   we're going to build the tree with
+    #   each node we list in ls
+    _, command, arg = line.split(' ')
+    if command == 'cd'
+      if arg == '..'
+        current = current.parent
+      elsif current.children.empty?
+        # stay put
+        # we haven't built this part of the tree yet
+        # should only happen on the first command ($ cd /)
+      else
+        current = current.children.find { |node| node.name == arg }
+      end
+    end
+  else
+    # build tree from output
+    # if dir
+    #   add new dir node
+    # if num
+    #   add new file node
+    x, name = line.split(' ')
+    if x == 'dir'
+      current.children << DirNode.new(name, current)
+    else
+      size = x.to_i
+      current.children << FileNode.new(name, current, size)
+    end
+  end
+end
+
+def find_directories_of_max_size(node, max_size)
+  directories = []
+  directories << node if node.size <= max_size
+  node.children.filter(&:directory?).each do |child|
+    directories += find_directories_of_max_size(child, max_size)
+  end
+  directories
+end
+
+puts find_directories_of_max_size(root, 100000).map(&:size).sum

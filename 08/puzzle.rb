32ccd6bed55108d49f8c3698df4749c272c07a7d
Brian <tachyons>
2022-12-08 13:04:14 -0800

    day 8 p2

--- 08/puzzle.rb
+++ 08/puzzle.rb
@@ -2,6 +2,35 @@ def tree_visible?(tree, i, row)
   row[0...i].all? { |el| tree > el } || row[i+1..-1].all? { |el| tree > el }
 end
 
+def visible_tree_score(tree, i, row)
+  p1 = []
+  p2 = []
+
+  finished = false
+  row[0...i].reverse.each do |el|
+    break if finished
+    if tree > el
+      p1 << el
+    elsif tree <= el
+      p1 << el
+      finished = true
+    end
+  end
+
+  finished = false
+  row[i+1..-1].each do |el|
+    break if finished
+    if tree > el
+      p2 << el
+    elsif tree <= el
+      p2 << el
+      finished = true
+    end
+  end
+
+  p1.count * p2.count
+end
+
 input = File.readlines('input').map &:strip
 
 grid = []
@@ -11,11 +40,12 @@ end
 
 inverted = grid.transpose
 
-count = 0
+score = 1
 1.upto(input.length-2).each do |i|
   1.upto(input.first.length-2).each do |j|
     tree = grid[i][j]
-    count += 1 if tree_visible?(tree, j, grid[i]) || tree_visible?(tree, i, inverted[j])
+    temp_score = visible_tree_score(tree, j, grid[i]) * visible_tree_score(tree, i, inverted[j])
+    score = temp_score if temp_score > score
   end
 end
-p count + (2 * grid.length) + 2 * (grid.first.length - 2)
+p score

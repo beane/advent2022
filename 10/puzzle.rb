ce8109171981fd4c4894c0fb61392530887cf6be
Brian <tachyons>
2022-12-12 11:44:01 -0800

    day 10 p2

--- 10/puzzle.rb
+++ 10/puzzle.rb
@@ -4,14 +4,15 @@ NOOP = 'noop'
 ADDX = 'addx'
 
 class CPU
-  attr_accessor :cycle, :x, :signal_strength_sum
+  attr_accessor :cycle, :x, :signal_strength_sum, :buffer
   def initialize
     @cycle=0
     @x=1
     @signal_strength_sum=0
+    @buffer=[]
   end
 
-  def tick(&block)
+  def tick1(&block)
     self.cycle+=1
     if (cycle-20) % 40 == 0
       self.signal_strength_sum += signal_strength
@@ -19,19 +20,41 @@ class CPU
     block.call if block
   end
 
+  def tick2(&block)
+    draw
+    self.cycle+=1
+    block.call if block
+  end
+
   def addx(n)
-    tick
-    tick do
+    tick2
+    tick2 do
       self.x+=n
     end
   end
 
   def noop
-    tick
+    tick2
   end
 
   def signal_strength
     self.cycle * self.x
+  end
+
+  def draw
+    hz_pos=(cycle%40)
+    char = sprite_range.include?(hz_pos) ? '#' : '.'
+    buffer[hz_pos] = char
+    flush if buffer.length == 40
+  end
+
+  def sprite_range
+    (x-1..x+1).to_a
+  end
+
+  def flush
+    puts buffer.join('')
+    self.buffer=[]
   end
 end
 
@@ -45,4 +68,7 @@ input.each do |line|
     cpu.addx(value.to_i)
   end
 end
-p cpu.signal_strength_sum
+# p1
+# p cpu.signal_strength_sum
+
+# p2 prints itself :)
